607b2fa5feaaa4436b957fd00a03c517f4a7d501
taylorcason90 <taylorcason90@gmail.com>
2024-02-26 23:25:31 -0500

    answers updated for submission

--- practicearrays.js
+++ practicearrays.js
@@ -186,19 +186,19 @@ let index = fruits.lastIndexOf("Apple");
 //--------------------------------------------------------------------------------
 
 
-//assignment practice code
+//assignment code
 // for loop that print 1 to 100
 
 // 
-//for (let i = 1; i < 101; i++) {
-  //  console.log(i);
-//}
+for (let i = 1; i < 101; i++) {
+   console.log(i);
+}
 
 // checking if divisible by a number 
 
 //if divisible by 3
 if (30 % 3 == 0) {
-    console.log("Fizz");
+    //console.log("Fizz");
   }else {
     console.log("30 is not divisible by 3");
   }
@@ -217,6 +217,91 @@ if (30 % 3 == 0) {
   if (30 % 5 == 0, 45 % 3 == 0) {
     console.log("Fizz Buzz");
   }
+
+
+  //-----------------------------------------------------------------------------
+  //-----------------------------------------------------------------------------
+
+                        //PART 2 LOOPS LAB
+
+//-------------------------------------------------------------------------------
+
+let i = 0;
+
+while (i < 3) {
+
+  console.log(i);
+
+   i++;}
+
+
+   let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+
+   if (i % 2 === 0) continue;
+
+   sum += i;
+
+}
+
+console.log(sum);
+
+// part 2 finding prime numbers examples
+
+// // example 1 
+ function isPrime(num) {
+     if (num <=1)  console.log("false");
+for (let n = 2; n < num; n++) {
+        if (num % n === 0) {
+           // console.log("false");
+        } else { 
+            console.log("true", num);
+            }
+    }
+    
+    
+        }
+
+//         isPrime(10)
+    
+    //     // example 2 
+
+        
+
+    //     function isPrime(num) {
+    //         if (num <=3)  console.log("false");
+    //    for (let n = 4; n < num; n++) {
+    //            if (num % n === 0) {
+    //                console.log("false");
+    //            } else { 
+    //                console.log("true", num);
+    //                }
+    //        }
+           
+           
+    //            }
+       
+    //            isPrime(7)
+
+
+               //
+
+
+    
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+
+// PART 3 LOOPS LAB
+
+
+const data1 = {id:"42", name:"Bruce", occupation:"Knight", age:"41"}
+const data2= {id:"57", name:"Bob", occupation:"Fry Cook", age:"19"}
+const data3= {id:"63", name:"Blaine", occupation:"Quiz Master", age:"58"}
+const data4= {id:"98", name:"Bill", occupation:"Doctor's Assistant", age:"26"}
+
+
+console.table([data1, data2, data3, data4]);
 
   
 
